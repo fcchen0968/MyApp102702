f3626b5b507b64b269cc723d051c8239549a1269
aaa <gdd>
2016-10-27 14:36:43 +0800

    time 1

--- MyApp102704/app/src/main/java/com/example/user/myapp102704/MainActivity.java
+++ MyApp102704/app/src/main/java/com/example/user/myapp102704/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         builer.show();
+    }
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入");
+        builder.setMessage("請輸入資料:");
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "輸入為:" + ed.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
     }
 }
