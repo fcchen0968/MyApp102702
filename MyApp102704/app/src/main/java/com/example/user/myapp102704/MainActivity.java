b789e14b114997a0830d25054cb635f19c4874d2
aaa <gdd>
2016-10-27 15:27:37 +0800

    time 2 修改按鍵殘值

--- MyApp102704/app/src/main/java/com/example/user/myapp102704/MainActivity.java
+++ MyApp102704/app/src/main/java/com/example/user/myapp102704/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.user.myapp102704;
 
+import android.support.v7.app.AppCompatActivity;
+
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int tmp=0, ch=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void click1(View v)
     {
@@ -53,16 +57,20 @@ public class MainActivity extends AppCompatActivity {
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("多選一");
+        tmp = 0;
         builder.setSingleChoiceItems(R.array.drinks, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                tmp = which;
             }
         });
         builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                String[] str = getResources().getStringArray(R.array.drinks);
+                TextView tv = (TextView) findViewById(R.id.textView);
+                ch = tmp;
+                tv.setText(str[ch]);
             }
         });
         builder.show();
