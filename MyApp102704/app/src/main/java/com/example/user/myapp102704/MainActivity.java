ecb85a5c21d5ab26a83441f31d8eba1929eb2f1b
aaa <gdd>
2016-10-27 14:17:57 +0800

    time 1

---
+++ MyApp102704/app/src/main/java/com/example/user/myapp102704/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.myapp102704;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        AlertDialog.Builder builer = new AlertDialog.Builder(MainActivity.this);
+        builer.setTitle("這是標題");
+        builer.setMessage("這是內文");
+        builer.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "按確定", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builer.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "按取消", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builer.show();
+    }
+}
